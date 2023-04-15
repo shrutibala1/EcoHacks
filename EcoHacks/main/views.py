f2b0f0f2753db385af8b885dc249e3c6a4362f10
nikhitag79 <nguntu@gmail.com>
2023-04-15 09:57:30 -0400

    first html page

--- EcoHacks/main/views.py
+++ EcoHacks/main/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'main/index.html')
 
 
 
