ec8ac0d585596dc308326eefae5fa058561cc340
Rushvi Patel <72409300+rushvipatel@users.noreply.github.com>
2023-04-19 08:08:55 -0400

    url

--- EcoHacks/main/urls.py
+++ EcoHacks/main/urls.py
@@ -6,6 +6,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', views.index, name='index'),
     path('main/go-map/', views.goToMap, name='add-item'),
-    path('main/index/', views.index, name='index'),
     path('main/about/', views.about, name = 'about'),
 ]
